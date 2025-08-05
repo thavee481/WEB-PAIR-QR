a50d69d4491d58c6dea06eb80a9c3f40c6da4a87
thavee481 <thaveeshaakalpa6@gmail.com>
2025-08-06 00:21:22 +0530

    Update pair.js

--- pair.js
+++ pair.js
@@ -42,7 +42,7 @@ async function SUHAIL() {
         });  
 
         if (!Smd.authState.creds.registered) {  
-            await delay(1200);  
+            await delay(1500);  
             num = num.replace(/[^0-9]/g, '');  
             const code = await Smd.requestPairingCode(num);  
             if (!res.headersSent) {  
