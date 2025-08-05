225b22e8cbf7331d3a52f834131fcce21cba4b2e
thavee481 <thaveeshaakalpa6@gmail.com>
2025-08-05 22:22:58 +0530

    Update mega.js

--- mega.js
+++ mega.js
@@ -1,7 +1,7 @@
 const mega = require("megajs");
 
 const auth = {
-    email: 'thaveeshaakalpa6@gmail.com',   //use your real vaild mega account email
+    email: 'akalphathaveesha19@gmail.com',   //use your real vaild mega account email
     password: 'Thaveesha@123',  ////use your real vaild mega account password
     userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'
 };
