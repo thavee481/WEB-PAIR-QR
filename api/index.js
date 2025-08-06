9d732d212a1b8633af89031d44ab4ae0f1eff531
thavee481 <thaveeshaakalpa6@gmail.com>
2025-08-07 00:48:44 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,4 +1,15 @@
-const app = require('../app');
-const serverless = require('serverless-http');
+// api/index.js
 
-module.exports = serverless(app);
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// Static files serve from public folder
+app.use(express.static(path.join(__dirname, '..', 'public')));
+
+// Serve HTML file on root route
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'public', 'pair.html'));
+});
+
+module.exports = app;
